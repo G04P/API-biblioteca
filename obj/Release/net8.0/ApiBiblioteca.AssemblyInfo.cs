9c549a6b4d1e4235319e3408fd5e2ed5837b2361
Gonzalo Perez <gperez@globaltask.net>
2024-08-19 11:44:08 -0300

    First commit

--- obj/Release/net8.0/ApiBiblioteca.AssemblyInfo.cs
+++ obj/Release/net8.0/ApiBiblioteca.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiBiblioteca")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c25c76357819f525c864657f59c6484f81dae421")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiBiblioteca")]
